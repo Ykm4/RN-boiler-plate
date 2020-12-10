06f56717669aebc898e0c0aa3d846afd066f1fe7
Yusuke Matsukuma <yusuke_matsukuma@bita.jp>
2020-12-11 07:39:27 +0900

    feat: cardコンポーネントの作成

---
+++ src/components/molecules/Card/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ArticleType } from '../../../services/news';
+import { TextImageBox } from '../TextImageBox';
+import { Button } from '../../atoms/Button';
+
+type Props = {
+  article: ArticleType;
+};
+
+export function Card({ article }: Props) {
+  return (
+    <View style={styles.root}>
+      <Text style={styles.title}>{article.title}</Text>
+      <Text style={styles.author}>{article.author}</Text>
+      <TextImageBox text={article.description} imageUrl={article.urlToImage} />
+      <View style={styles.buttonContainer}>
+        <Button />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#FAFAFA',
+    borderColor: '#FAFAFA',
+    borderWidth: 1,
+    marginVertical: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '700',
+    marginBottom: 4,
+  },
+  author: {
+    fontSize: 12,
+    marginBottom: 4,
+  },
+  imgCard: {},
+  buttonContainer: {
+    marginTop: 6,
+  },
+});
